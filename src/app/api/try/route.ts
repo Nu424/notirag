6eaf95c32453ce278cdc42b3f7091aeda27de26a
Nuva424 <murataka0424@gmail.com>
2025-06-14 18:28:10 +0900

    add: RouteHandlerを軽く用意し、今後の実装内容を文字に起こした

---
+++ src/app/api/try/route.ts
@@ -0,0 +1,62 @@
+import { NextResponse } from 'next/server';
+
+// GET /api/try
+export async function GET(request: Request) {
+    console.log(request);
+    const users = [{ id: 1, name: 'John Doe' }];
+    return NextResponse.json(users);
+}
+
+// POST /api/try
+export async function POST(request: Request) {
+    const data = await request.json();
+    // ... データベースへの保存処理など
+    return NextResponse.json({ message: 'User created', data }, { status: 201 });
+}
+
+/*
+# Notionデータベースとの連携ToDo
+
+## Notion形式<->md形式の変換
+### 入力
+- Notion形式のデータ or md形式のデータ
+### 出力
+- md形式のデータ or Notion形式のデータ
+### メモ
+- Notion->mdは、
+- md->Notionは、
+
+## データベースの取得
+### 入力
+- データベースID
+- APIキー
+### 出力
+- データベースの各ページの情報(オブジェクトとして取得)
+
+## ページの取得
+### 入力
+- ページID
+- APIキー
+### 出力
+- ページのデータ(タイトル、キーワードフィールド、中身)
+    - ページの中身は、md形式で出力する
+
+## ページの更新
+### 入力
+- ページID
+- APIキー
+- 更新内容
+    - 更新内容は、md形式で入力する
+### 出力
+- 更新後のページのデータ(タイトル、キーワードフィールド、中身)
+
+## ページの作成
+### 入力
+- データベースID
+- APIキー
+- 作成内容
+    - 作成内容は、md形式で入力する
+### 出力
+- 作成後のページのデータ(タイトル、キーワードフィールド、中身)
+
+*/
